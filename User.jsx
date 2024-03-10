f2063cddcef9af57013437f2967dcb61c79a68ed
shivam <shivamnegi479@gmail.com>
2024-03-10 23:54:13 +0530

    jsx ie

---
+++ User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      hello this is example of jsx
+      we can directoly write the js code inside html or write html inside js like {5*5} {console.log("hello this is example of jsx")}
+    </div>
+  )
+}
+
+export default User
--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import {User,User2} from './User';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <User />
-      <User2 />
-      <User22 />
+
     </div>
   );
 }
-function User22() {
-  return <h1>
-    Hello function
-  </h1>
-  }
+
 
 export default App;
